81f75c1a30da70f3be110240d20ec3f248e201db
Frédérique-Annie Cyr <facyr3@ulaval.ca>
2026-09-25 13:17:42 -0400

    added sceneprimitive class

---
+++ src/primitive/scenePrimitive.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "sceneObject.h"
+#include "ofMain.h"
+
+class ScenePrimitive : public SceneObject {
+public:
+	ofColor fillColor = ofColor::white;
+	ofColor lineColor = ofColor::black;
+	float lineWidth = 1.0f;
+	bool filled = true;
+
+	virtual ~ScenePrimitive() = default;
+
+	protected:
+		void applyStyle() const {
+			ofSetLineWidth(lineWidth);
+			if (filled) {
+				ofFill();
+				ofSetColor(fillColor);
+			} else {
+				ofNoFill();
+				ofSetColor(lineColor);
+			}
+	}
+};
